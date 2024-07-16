f018c9d59eb280d57c5c7f28cc9cbb44524972bc
Osama Sowidan <124528987+sowidan1@users.noreply.github.com>
2024-07-16 22:31:45 +0300

    Update ProfileUpdateRequest.php

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -17,7 +17,7 @@ class ProfileUpdateRequest extends FormRequest
     {
         return [
 
-            // dwdwdwd
+            // dwdwdwd wffwfwf
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
         ];
